81c8179b83a1250d3b65253c9a8ab9865f1a3008
Matteo Abis <gmatteo..abis@gmail.com>
2014-04-10 16:03:17 +0200

    removed xrange

--- dpc_reconstruction/flats_every.py
+++ dpc_reconstruction/flats_every.py
@@ -18,7 +18,7 @@ def chunks(l, n):
     """ Yield successive n-sized chunks from l.
     http://stackoverflow.com/a/312464
     """
-    for i in xrange(0, len(l), n):
+    for i in range(0, len(l), n):
         yield l[i:i + n]
 
 
