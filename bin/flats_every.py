a0e93ab6745bd58c7a8e7eeb41840463787879a1
Matteo Abis <gmatteo..abis@gmail.com>
2016-07-19 14:28:14 +0200

    dpc_reconstruction works

--- bin/flats_every.py
+++ bin/flats_every.py
@@ -11,11 +11,13 @@ import logging.config
 import click
 import tensorflow as tf
 import numpy as np
+import h5py
 
 import dpc_reconstruction
 import dpc_reconstruction.io.hdf5 as hdf5
 import dpc_reconstruction.phase_stepping as phase_stepping
-from dpc_reconstruction.logger_config import config_dictionary
+import dpc_reconstruction.logger_config as logger_config
+
 log = logging.getLogger()
 
 
@@ -44,6 +46,9 @@ def main(
         overwrite,
         verbose,
         jobs=1):
+    logging.config.dictConfig(
+        logger_config.get_dict(verbose)
+    )
     n = flats_every + n_flats
     samples = []
     flats = []
@@ -62,27 +67,45 @@ def main(
                 chunk_flats = np.tile(
                     chunk_flats, (chunk_samples.shape[0], 1, 1)
                 )
-                samples.append(chunk_samples)
-                flats.append(chunk_flats)
-                samples = np.concatenate(samples)
-                flats = np.concatenate(flats)
-                output_name = hdf5.output_name(files)
-                with tf.Session() as sess:
-                    sample_tensor = tf.placeholder(
-                        tf.float32, shape=samples.shape)
-                    flat_tensor = tf.placeholder(
-                        tf.float32, shape=flats.shape)
-                    sample_signals = phase_stepping.get_signals(sample_tensor)
-                    flat_signals = phase_stepping.get_signals(flat_tensor)
-                    dpc_reconstruction = phase_stepping.compare_sample_to_flat(
-                        sample_signals,
-                        flat_signals
-                    )
-                    visibility = phase_stepping.visibility(flat_signals)
-                    dpc_numpy_array = sess.run(
-                        dpc_reconstruction,
-                        feed_dict={
-                            sample_tensor: samples,
-                            flat_tensor: flats,
-                        })
-                    print(dpc_numpy_array.shape)
+        samples.append(chunk_samples)
+        flats.append(chunk_flats)
+    samples = np.concatenate(samples)
+    flats = np.concatenate(flats)
+    output_name = hdf5.output_name(files)
+    with tf.Session() as sess:
+        sample_tensor = tf.placeholder(
+            tf.float32, shape=samples.shape)
+        flat_tensor = tf.placeholder(
+            tf.float32, shape=flats.shape)
+        sample_signals = phase_stepping.get_signals(sample_tensor)
+        flat_signals = phase_stepping.get_signals(flat_tensor)
+        dpc_reconstruction = phase_stepping.compare_sample_to_flat(
+            sample_signals,
+            flat_signals
+        )
+        visibility = phase_stepping.visibility(flat_signals)
+        dpc_np, flat_np, visibility_np = sess.run(
+            [
+                dpc_reconstruction,
+                flat_signals,
+                visibility,
+            ],
+            feed_dict={
+                sample_tensor: samples,
+                flat_tensor: flats,
+            })
+        log.debug("dpc_reconstruction dataset with shape %s", dpc_np.shape)
+        log.debug("flat_parameters dataset with shape %s", flat_np.shape)
+        log.debug("visibility dataset with shape %s", visibility_np.shape)
+        log.debug("flat_phase_stepping_curves dataset with shape %s",
+                  flats.shape)
+        log.debug("phase_stepping_curves dataset with shape %s", flats.shape)
+        log.debug("saving to %s", output_name)
+        with h5py.File(output_name, "w-") as output_file:
+            group = output_file.create_group("postprocessing")
+            group.create_dataset("dpc_reconstruction", data=dpc_np)
+            group.create_dataset("visibility", data=visibility_np)
+            group.create_dataset("flat_phase_stepping_curves", data=flats)
+            group.create_dataset("flat_parameters", data=flat_np)
+            group.create_dataset("phase_stepping_curves", data=samples)
+            log.info("saved data to %s", output_name)
